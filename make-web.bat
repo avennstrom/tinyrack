01ef19914e24c4fa916dcf28b5c8e02d548bb99a
Andreas Vennström <6706447+avennstrom@users.noreply.github.com>
2025-09-17 23:33:34 +0200

    remove old build script

--- make-web.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-if not exist bin mkdir bin
-copy asset\font.png bin
-copy src\audio-processor.js bin
-python fontgen.py
-emcc -std=c23 -Os -o bin/tinyrack.html --shell-file src/emscripten_shell.html --js-library src/platform.js src/main.c src/modules.c src/timer.c src/parser.c src/renderbuf.c src/platform_web.c src/strbuf.c -sINITIAL_MEMORY=128MB -D__DEFINED_timer_t -DPLATFORM_WEB -sFILESYSTEM=0 -sMINIMAL_RUNTIME=0 -sASSERTIONS=0 --closure 0
